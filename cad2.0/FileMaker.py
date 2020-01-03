140be8679082717240856094d6f79b8a08bcb827
faashraf <faiqashraf32@gmail.com>
2020-01-02 20:55:02 -0600

    wowza, lots of changes related to cad

--- cad2.0/FileMaker.py
+++ cad2.0/FileMaker.py
@@ -1,36 +1,11 @@
-# Python program to explain shutil.move() method  
-      
-# importing os module  
-import os  
-  
-# importing shutil module  
-import shutil  
-  
-# path  
-path = 'C:/Users/faiqe/OneDrive/Desktop'
-  
-# List files and directories  
-# in 'C:/Users/Rajnish/Desktop/GeeksforGeeks'  
-print("Before moving file:")  
-print(os.listdir(path))  
-  
-  
-# Source path  
-source = 'C:/Users/faiqe/OneDrive/Desktop/cad2.0'
-  
-# Destination path  
-destination = 'C:/Users/faiqe/OneDrive/Desktop/cad2.0/files/cars'
-  
-# Move the content of  
-# source to destination 
-# using shutil.copytree() as parameter 
-dest = shutil.move(source, destination, copy_function = shutil.copytree)  
-  
-# List files and directories  
-# in "C:/Users / Rajnish / Desktop / GeeksforGeeks"  
-print("After moving file:")  
-print(os.listdir(path))  
-  
-# Print path of newly  
-# created file  
-print("Destination path:", dest)  
+import os
+
+filename = input("Enter a file name >> ")
+text = input("Enter text to save to the file >> ")
+
+dirpath = os.getcwd()
+print(dirpath)
+
+a = open("files/cars/" + filename + "_car.txt", "a")
+a.write(text)
+a.close()
