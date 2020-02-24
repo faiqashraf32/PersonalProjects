fec01068b795db29ff6abe1c8f95b2a35bbf1127
faashraf <faiqashraf32@gmail.com>
2020-02-24 01:29:52 -0500

    finished product with classes and defs

--- grader/grader.py
+++ grader/grader.py
@@ -1,55 +1,99 @@
 class Grader:
-    def grade(self):
-        # get number of questions
-        numOfQuestions = input("How many questions are there? >> ")
 
+    def start(self):
+        # greeting message
+        o = input("Welcome to gradermatic.\nTo enter responses, enter 1.\nTo enter an answer key, enter 2.\nTo grade, enter 3.\nTo change a response, enter 4.\nPress any other key to quit. >> ")
+        if o == "1":
+            # enter responses
+            self.numOfQuestions = input("How many questions are there? >> ")
+            a.enterResponses()
+        elif o == "2":
+            # enter answer key
+            a.answerKey()
+        elif o == "3":
+            # grade the exam
+            a.grade()
+        # elif 0 == "4":
+        #     a.changeAns()
+        else:
+            exit(0)
+        # get number of questions
+    
+    def enterResponses(self):
         # start gathering answers
         i = 1
-        responses = []
-        while i <= int(numOfQuestions):
+        self.responses = []
+        while i <= int(self.numOfQuestions):
             entry = input("\nWhat is your answer for question #" + str(i) + " >> ")
-            responses += entry
+            self.responses += entry
             i += 1
         
         # display user responses
+        print("\n\n\n")
         print("\nResponses:\n")
         j = 1
-        for r in responses:
+        for r in self.responses:
             print(str(j) + ". " + r)
             j+=1
         # change any answers?
         # change = input("Would you like to change any answers? 1 = yes, 2 = no")
         # if change == "1":
         #     a.changeAns()
-        
-        # grade the responses
+        print("\n\n\n")
+        a.start() # take us back to the main menu
+    
+    def answerKey(self):
         # input answer key
         x = 1
-        answers = []
-        while x <= int(numOfQuestions):
+        self.answers = []
+        while x <= int(self.numOfQuestions):
             aentry = input("\nWhat is the correct answer for question #" + str(x) + " >> ")
-            answers += aentry
+            self.answers += aentry
             x += 1
         
         # display answer key
+        print("\n\n\n")
         print("\nResponses:\n")
         y = 1
-        for z in answers:
+        for z in self.answers:
             print(str(y) + ". " + z)
             y+=1
+        print("\n\n\n")
+        a.start()
 
+    def grade(self):
+        # grade the responses
         # time to actually grade the exam
         numCorrect = 0
-        for p, q in zip(responses, answers):
+        self.incorrect = []
+        for p, q in zip(self.responses, self.answers):
             if p == q:
+                # correct answer, so add 1 to their score
                 numCorrect += 1
+            else:
+                # incorrect answer, let us record this instance to show them that they entered something in wrong
+                #                tells us what problem number                    user entry                  correct answer
+                print("index of self.responses.index: "+str(self.responses.index(p)))
+                # since computers start counting at 0, we must increment self.responses.index(p) by 1, or else our "wrong answer" will be attributed to the wrong question
+                zebra = self.responses.index(p) + 1
+                goingIn = "#" + str(zebra) + " user entered: " + p + " correct answer: " + q
+                self.incorrect.append(goingIn) # put it all together in 1 list position
+                goingIn = "" # reset, incase we need to add more stuff
+
                 
         # issue a grade
         print(str(numCorrect))
-        print("Number of correct answers = " + str(numCorrect) + " out of " + str(numOfQuestions))
-        grade = int(numOfQuestions) - int(numCorrect)
-        grade = grade / int(numOfQuestions)
+        print("Number of correct answers = " + str(numCorrect) + " out of " + str(self.numOfQuestions))
+        grade = int(numCorrect) / int(self.numOfQuestions)
         print("Your score is: " + str(grade))
+
+        # display incorrect answers:
+        print("Here are the questions the user got wrong, as well as their correct answers:")
+        for l in self.incorrect:
+            print(l)
+
+        # end the program
+        exit(0)
 
         # def changeAns(self):
         #     # time to change answers
@@ -63,4 +107,4 @@ class Grader:
 
 if __name__ == "__main__":
     a = Grader()
-    a.grade()
+    a.start()
