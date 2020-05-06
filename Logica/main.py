3e71c6f0b40b729ed750a4ebd935db14cc72ee8d
faashraf <faiqashraf32@gmail.com>
2020-05-05 23:40:14 -0500

    data entry issue, waiting for response on stack

--- Logica/main.py
+++ Logica/main.py
@@ -7,7 +7,14 @@ class Logica:
         # seek user input
         # a,b,c,d,e,f = [],[],[],[],[],[]
         self.a = []
-        setEdit = input("Which set would you like to edit? (a-f) >> ")
+        self.b = []
+        self.c = []
+        self.d = []
+        self.e = []
+        self.f = []
+        self.u = [] # universal set
+
+        setEdit = input("Which set would you like to edit? (a-f) or universe (u) >> ")
         if setEdit == "a":
             print("Editing set a. Please enter values, when finished, enter 'stop' \n")
             entry = ""
@@ -23,9 +30,28 @@ class Logica:
                         # print(str(response) + ",")
                         aa += str(response)
                     print(aa)
-                    exit(0)
+                    a.whatNow()
 
-            
+        elif setEdit == "b":
+            print("Editing set b. Please enter values, when finished, enter 'stop' \n")
+            entry = ""
+            while True:
+                entry = input("give me something >>> ")
+                if entry != "stop": 
+                    self.b += entry
+                    print("I have added " + entry + " to the set b.\n")
+                else:
+                    # display values in b
+                    bb = []
+                    for response in self.b:
+                        # print(str(response) + ",")
+                        bb += str(response)
+                    print(bb)
+                    a.whatNow()
+
+    def whatNow(self):
+        print("Here are your options: \n")
+        exit(0)       
         
 
 
