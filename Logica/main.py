4533b7581eb7a3bd29c9c33bc9983c7a48c0e533
faashraf <faiqashraf32@gmail.com>
2020-05-05 23:18:34 -0500

    "a" set can be edited

--- Logica/main.py
+++ Logica/main.py
@@ -1,6 +1,32 @@
 class Logica:
+
     def start(self):
-        print("Hello world!")
+        # welcome user
+        print("Welcome to Logica!\n")
+
+        # seek user input
+        # a,b,c,d,e,f = [],[],[],[],[],[]
+        self.a = []
+        setEdit = input("Which set would you like to edit? (a-f) >> ")
+        if setEdit == "a":
+            print("Editing set a. Please enter values, when finished, enter 'stop' \n")
+            entry = ""
+            while True:
+                entry = input("give me something >>> ")
+                if entry != "stop": 
+                    self.a += entry
+                    print("I have added " + entry + " to the set a.\n")
+                else:
+                    # display values in a
+                    aa = []
+                    for response in self.a:
+                        # print(str(response) + ",")
+                        aa += str(response)
+                    print(aa)
+                    exit(0)
+
+            
+        
 
 
 if __name__ == "__main__":
