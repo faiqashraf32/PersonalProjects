82108a9294416e50bfdc7f5dbda6c18816050db6
faashraf <faiqashraf32@gmail.com>
2020-07-10 18:38:44 -0500

    added new project

---
+++ onTheClock/onTheClock.py
@@ -0,0 +1,16 @@
+
+class On:
+   def main(self):
+       print("\nClock in - 1, Clock out - 2")
+
+   def clockIn(self):
+       pass
+   def clockOut(self):
+       pass    
+ 
+
+
+
+if __name__ == "__main__":
+    a = On()
+    a.main()
