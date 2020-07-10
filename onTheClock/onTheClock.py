4a66d5dcdc6b9bc604c20b9f54d195c73e0942aa
faashraf <faiqashraf32@gmail.com>
2020-07-10 18:47:46 -0500

    making progress

--- onTheClock/onTheClock.py
+++ onTheClock/onTheClock.py
@@ -1,10 +1,27 @@
+from datetime import datetime
 
 class On:
+   log = "logs/log.txt"
    def main(self):
-       print("\nClock in - 1, Clock out - 2")
+       #name = input("Enter your full name >> ")
+       z = input("\nClock in - 1, Clock out - 2 >> ")
+       if z == "1":
+           a.clockIn()
+       if z == "2":
+           a.clockOut() 
+           
 
    def clockIn(self):
-       pass
+       # record system date and time
+       clockInDateTime = str(datetime.now())
+       
+       # save to text file
+
+
+       # tell the user that they are clocked in
+       print("Clocked in")
+
+
    def clockOut(self):
        pass    
  
