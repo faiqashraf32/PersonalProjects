048b78f187c11d4b70aaf126657f4bd5e81e7692
faashraf <faashraf@iu.edu>
2022-01-16 17:53:18 -0600

    runs indefinetly

--- abc/abc.py
+++ abc/abc.py
@@ -1,16 +1,20 @@
+from os import kill
 import string
-
-# get user input
-inp = input("Enter a word you want represented with numbers >>> ")
-
-# convert input to lowercase
-inp = inp.lower()
-# converts input into list so that we can individually change each number
-outList = []
-for x in inp:
-    # figures out corresponding number to each letter in inp
-   outList.append(ord(x) - 96)
-# converts list to string so that it shows up normally for user
-out = ''.join(str(e) for e in outList)
-# print(outList)
-print("Your entry of ",inp," corresponds to ", out , " in number form.")
+import sys
+while True:
+    # get user input
+    inp = input("Enter a word you want represented with numbers (enter '-' to end) >>> ")
+    if inp is "-":
+        sys.exit()
+    else:
+        # convert input to lowercase
+        inp = inp.lower()
+        # converts input into list so that we can individually change each number
+        outList = []
+        for x in inp:
+            # figures out corresponding number to each letter in inp
+            outList.append(ord(x) - 96)
+        # converts list to string so that it shows up normally for user
+        out = ''.join(str(e) for e in outList)
+        # print(outList)
+        print("Your entry of ",inp," corresponds to ", out , " in number form.")
