7120b33e05bcd145799d79047866732f7f7bea51
faashraf <faashraf@iu.edu>
2022-01-16 14:17:07 -0600

    added comments

--- abc/abc.py
+++ abc/abc.py
@@ -8,7 +8,9 @@ inp = inp.lower()
 # converts input into list so that we can individually change each number
 outList = []
 for x in inp:
+    # figures out corresponding number to each letter in inp
    outList.append(ord(x) - 96)
+# converts list to string so that it shows up normally for user
 out = ''.join(str(e) for e in outList)
 # print(outList)
 print("Your entry of ",inp," corresponds to ", out , " in number form.")
