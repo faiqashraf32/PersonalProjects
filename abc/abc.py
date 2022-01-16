1d1a9fc89fadb4b011ac0f1d5111f4a8de1ef229
faashraf <faashraf@iu.edu>
2022-01-16 14:16:17 -0600

    working product

--- abc/abc.py
+++ abc/abc.py
@@ -1,2 +1,14 @@
+import string
+
 # get user input
 inp = input("Enter a word you want represented with numbers >>> ")
+
+# convert input to lowercase
+inp = inp.lower()
+# converts input into list so that we can individually change each number
+outList = []
+for x in inp:
+   outList.append(ord(x) - 96)
+out = ''.join(str(e) for e in outList)
+# print(outList)
+print("Your entry of ",inp," corresponds to ", out , " in number form.")
